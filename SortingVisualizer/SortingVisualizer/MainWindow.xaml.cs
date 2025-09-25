4e884f0f8104b8a5b3af0d54d19750372929cec2
Ibragim <i.zharkynbai@entwicklerburg.com>
2025-09-25 16:50:58 +0600

    upgrade MainWindow, add buttons and draw function

--- SortingVisualizer/SortingVisualizer/MainWindow.xaml.cs
+++ SortingVisualizer/SortingVisualizer/MainWindow.xaml.cs
@@ -1,28 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Always use English comments
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SortingVisualizer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private int[] array; // The array we will visualize
+        private Random rand = new Random(); // Random generator
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); // Connects XAML with this C# file
+            GenerateArray((int)ArraySizeSlider.Value);
+
+            // Button events
+            ResetButton.Click += (s, e) => GenerateArray((int)ArraySizeSlider.Value);
+            StartButton.Click += (s, e) => StartSorting();
+        }
+
+        // Generate a new random array
+        private void GenerateArray(int size)
+        {
+            array = Enumerable.Range(1, size).OrderBy(x => rand.Next()).ToArray();
+            DrawArray();
+        }
+
+        // Draw array as rectangles in Canvas
+        private void DrawArray()
+        {
+            ArrayCanvas.Children.Clear();
+            double canvasWidth = ArrayCanvas.ActualWidth == 0 ? 700 : ArrayCanvas.ActualWidth;
+            double canvasHeight = ArrayCanvas.ActualHeight == 0 ? 400 : ArrayCanvas.ActualHeight;
+
+            double barWidth = canvasWidth / array.Length;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                double barHeight = (array[i] / (double)array.Length) * canvasHeight;
+                Rectangle rect = new Rectangle
+                {
+                    Width = barWidth - 2,
+                    Height = barHeight,
+                    Fill = Brushes.SteelBlue
+                };
+                Canvas.SetLeft(rect, i * barWidth);
+                Canvas.SetBottom(rect, 0);
+                ArrayCanvas.Children.Add(rect);
+            }
+        }
+
+        // Start sorting (placeholder for now)
+        private void StartSorting()
+        {
+            MessageBox.Show($"Algorithm selected: {(AlgorithmComboBox.SelectedItem as System.Windows.Controls.ComboBoxItem)?.Content}");
         }
     }
 }
